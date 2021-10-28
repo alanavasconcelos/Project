be206469d8a98702d2d1836c0df408d2e7f5f26d
MathhAraujo <92542444+MathhAraujo@users.noreply.github.com>
2021-10-27 22:21:23 -0300

    Atributos
    Roteiro 10 - Identifique e implemente os atributos de cada classe.

---
+++ JogoLPOO/src/Tabuleiro.java
@@ -0,0 +1,11 @@
+
+public class Tabuleiro {
+	
+	private int larguraDasCasas;
+	private int alturaDasCasas;
+	private int numeroDeBombas;
+	private	int bombasProximas;
+	private int casasSemBombasProximas;
+	private int timer;
+	
+}
