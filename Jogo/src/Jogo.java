f12e71a152f5dcba836752d08d105d1a3663ea65
alana <alana@LAPTOP-1OG6E303>
2021-11-03 17:19:53 -0300

    Identifique as operações nas diferentes classes criadas.

--- Jogo/src/Jogo.java
+++ Jogo/src/Jogo.java
@@ -1,4 +1,43 @@
 
 public class Jogo {
+	
+    
+	private boolean terminou = false;
+	private boolean ganhou = false;
+	private int[] jogada;
+	private int rodada = 0;
+	private Tabuleiro board;
+	
+	/*private int dificuldade;
+	private int numeroDeBandeirasTotais;
+	private int numeroDeBandeirasAtivas;
+	private String nomeDoJogador;
+	private boolean perdeuJogo;*/
+	
+	public Jogo(){
+		
+	}
+	public void Jogar(Tabuleiro board){
+		
+	}
+	
+    /*
+	public int getDificuldade() {
+
+		return this.dificuldade;
+
+	}
+
+	public void setNomeDoJogador(String nomeDoJogador) {
+
+		this.nomeDoJogador = nomeDoJogador;
+
+	}
+
+	public String getNomeDoJogador() {
+
+		return this.nomeDoJogador;
+
+	}*/
 
 }
