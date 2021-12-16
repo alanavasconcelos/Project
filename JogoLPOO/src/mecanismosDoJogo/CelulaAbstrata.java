65bc238ee5f1f11b62cc3932e797756bf0320890
MathhAraujo <92542444+MathhAraujo@users.noreply.github.com>
2021-12-16 13:54:31 -0300

    Checkpoint 4 - Ranking incompleto
    Adicionado todos os pontos do checkpoint 4
    Adicionada opção para escolher entre a interface gráfica e o console
    A interface gráfica está apenas com o jogo básico em 5x5
    Console está com todas as opções do roteiro, com exceção de um ranking finalizado

--- JogoLPOO/src/mecanismosDoJogo/CelulaAbstrata.java
+++ JogoLPOO/src/mecanismosDoJogo/CelulaAbstrata.java
@@ -1,152 +1,176 @@
 package mecanismosDoJogo;
 
-public abstract class CelulaAbstrata {
-	
+import java.io.Serializable;
+
+public abstract class CelulaAbstrata implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private boolean temMina;
 	private boolean clicada;
 	private boolean temBandeira;
 	private boolean ehMaluca;
-	
+
 	public CelulaAbstrata() {
-		
+
 		this.temMina = false;
 		this.clicada = false;
-		this.temBandeira =  false;
+		this.temBandeira = false;
 		this.ehMaluca = false;
-		
+
 	}
-	
-	//método para minar, se não tiver minada ele mina e retorna true, caso já tenha mina retorna false
+
+	// Métodos relacionados as vizinhas precisam ser passados para a subclasse
+	public abstract int numMinasNasVizinhas();
+
+	public abstract int clicar();
+
+	public abstract void limparVizinhas();
+
+	public abstract int clicarAlternativo();
+
 	public boolean colorcarMina() {
-			
+
 		if (!this.getTemMina()) {
-	        	
+
 			this.setTemMina(true);
 			return true;
-	            
+
 		} else {
-	        	
-			return false;  
-			
+
+			return false;
+
 		}
-		
+
 	}
-		
-	//permite que ele marque e desmarque a bandeira
-	public abstract int marcar();	
-		
-	//esse for percorre as vizinhas de uma celula,
-	//verificando se ela tem minas ao redor, e conta quantas minas e retorna o numero de minas ao redor que essa celula tem
-	public abstract int numMinasNasVizinhas();
-		
-	//esse metodo é para clicar, caso ele clique e tenha uma mina retorna -1 (bom para fazermos o teste no console)
-	// e caso ele clique e não tenha uma mina ele retorna o numero de minas que tem ao seu redor (que são os números, as dicas que aparecem no jogo)
-	public abstract int clicar() ;
-	
-	public abstract void limparVizinhas();
-		
+
+	// Método para colocar e retirar bandeiras
+	public int marcar() {
+
+		if (!this.getClicada()) {
+
+			boolean temBandeira = this.getTemBandeira();
+
+			if (temBandeira == true) {
+
+				this.setTemBandeira(!temBandeira);
+				return 1;
+
+			} else {
+
+				this.setTemBandeira(!temBandeira);
+				return -1;
+
+			}
+
+		}
+
+		return 0;
+
+	};
+
 	public void reset() {
-			
+
 		this.setTemMina(false);
 		this.temBandeira = false;
 		this.setClicada(false);
-	        
+
 	}
-		
+
 	public boolean finalizado() {
-			
-		if(this.getTemMina() && this.temBandeira || this.getTemMina() && !this.getClicada()) { 
-	        	
+
+		if (this.getTemMina() && this.getTemBandeira() || this.getTemMina() && !this.getClicada()) {
+
 			return true;
-	        
+
 		}
-	        
-		if(!this.getTemMina() && !this.temBandeira && this.getClicada()) {
-	        
+
+		if (!this.getTemMina() && !this.getTemBandeira() && this.getClicada()) {
+
 			return true;
-	        	
+
 		}
-	        
-			return false;
-	        
+
+		return false;
+
 	}
-		
+
 	public void setClicada(boolean clicada) {
-			
+
 		this.clicada = clicada;
-			
+
 	}
-		
+
 	public boolean getClicada() {
-			
+
 		return clicada;
-			
+
 	}
 
 	public void setTemMina(boolean temMina) {
-			
+
 		this.temMina = temMina;
-			
+
 	}
 
 	public boolean getTemMina() {
-			
+
 		return temMina;
-			
+
 	}
-	
+
 	public void setTemBandeira(boolean temBandeira) {
-		
+
 		this.temBandeira = temBandeira;
-			
+
 	}
-	
+
 	public boolean getTemBandeira() {
-			
+
 		return this.temBandeira;
-			
+
 	}
-	
+
 	public void setEhMaluca(boolean ehMaluca) {
-		
+
 		this.ehMaluca = ehMaluca;
-		
+
 	}
-	
+
 	public boolean getEhMaluca() {
-		
+
 		return this.ehMaluca;
-		
+
 	}
-	
-	
+
 	public String toString() {
-			 
-		if(this.temBandeira){
-		      		
+
+		if (this.temBandeira) {
+
 			return "T";
-		      		
+
 		}
-			 
-		if(this.clicada) {
-				 
+
+		if (this.clicada) {
+
 			if (this.temMina) {
-		    	   
+
 				return "B";
-		            
-			}else {
-				
+
+			} else {
+
 				return "" + this.numMinasNasVizinhas();
-				
+
 			}
-		       
-		       
+
 		} else {
-		    	
+
 			return "C";
-		    	
+
 		}
-			 
+
 	}
-	 
+
 }
